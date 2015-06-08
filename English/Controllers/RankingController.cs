1595ae5abe2b07745896daa7bf23020ec86150c2
lukaszmaly <lukaszstachowiak22@gmail.com>
2015-06-08 15:03:13 +0200

    Dodano ranking użytkowników

---
+++ English/Controllers/RankingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using English.Models;
+
+namespace English.Controllers
+{
+    public class RankingController : Controller
+    {
+        private EnglishContext db = new EnglishContext();
+
+        // GET: Ranking
+        public ActionResult Index()
+        {
+            return View(db.GameUsers.OrderByDescending(user=> user.Points).ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
